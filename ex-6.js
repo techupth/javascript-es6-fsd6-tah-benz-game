f1d895346103cc6541130035ab8cfc86b788941b
Your Name <you@example.com>
2024-05-24 14:33:35 +0700

    edit ex1-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,4 @@
-let height = undefined;
-let result;
+let height = undefined ?? "Height is not defined";
+let result = height;
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
